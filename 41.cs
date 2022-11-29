b186623d48a1f692d0d3cebce63915206a8e877f
Eva <eva.leona@mail.ru>
2022-11-29 12:31:02 +0300

    41

---
+++ 41.cs
@@ -0,0 +1,23 @@
+﻿int[] myArray = InputArray();
+Console.WriteLine($"{ArrayToString(myArray)} -> {GetPisitiveCount(myArray)}");
+
+int GetPisitiveCount(int[] array) 
+{
+    int count = 0;
+    foreach (int item in array) { if (item > 0) count++; }
+    return count;
+}
+
+int[] InputArray() 
+{
+    Console.Write("Введите массив из N чисел, разделяя запятыми: ");
+    string[] numbers = Console.ReadLine()!.Trim(' ',',').Split(",");
+    int[] array = new int[numbers.Length];
+    int i = 0;
+    foreach (var item in numbers)
+    {
+        array[i] = Convert.ToInt32(item); i++;
+    }
+    return array;
+}
+String ArrayToString(int[] array) { return "[" + String.Join(", ", array) + "]"; }
